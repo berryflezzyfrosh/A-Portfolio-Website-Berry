fdc579af36ab88d818c235f886c4215aa2d005bb
Berry <berryflezzyfrosh@gmail.com>
2024-11-29 04:27:30 -1200

    last commit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<script src="https://unpkg.com/type.js@2.0.15/dist/typed.umd.js"></script>
 var typed3 = new typed3(".text", {
     Strings: ["Frontend Developer", "YouTuber" , "Web Developer"],
     typeSpeed: 100,
